495be93401b516bae645c7cead7d02d63fe4c227
Ernest Rudziec <ernest.rudziec@tdy.pl>
2021-08-05 02:46:50 +0200

    fixes style

--- components/Layout.js
+++ components/Layout.js
@@ -1,10 +1,15 @@
 import React from "react";
 import { Footer } from "./Footer";
+import Link from "next/link";
 
 export default function Layout({ children }) {
   return (
     <div className="layout-container">
-      <img src="/assets/logo.svg" />
+      <Link href="/">
+        <a className="logo-link">
+          <img src="/assets/logo.svg" />
+        </a>
+      </Link>
       {children}
       <Footer />
     </div>
